747d0a5ee0b17af5cde60fbcf479d413055254ff
dsoldatow <dsoldatov26@gmail.com>
2019-11-24 19:24:24 +0700

    arch

---
+++ app/config/config.py
@@ -0,0 +1,3 @@
+from config.private import *
+VK_API_VERSION = 5.103
+VK_GROUP_ID = 1321
--- app/photo_api.py
+++ app/photo_api.py
@@ -1,11 +1,32 @@
-
+import config as cfg
+import requests
 
 class PhotoAPI:
+    """
+    Класс для работы с фотографиями в ВК
+    """
 
     @staticmethod
-    def get_photo_by_albums(**kwargs):
+    def create_album(**kwargs):
         pass
 
     @staticmethod
-    def add_photo_by_albums(**kwargs):
+    def get_all_photos_by_album(**kwargs):
         pass
+
+    @staticmethod
+    def get_photo_by_url(**kwargs):
+        pass
+
+    @staticmethod
+    def add_photo_in_album(**kwargs):
+        pass
+
+    @staticmethod
+    def get_url_for_add(**kwargs):
+        json = {
+            'album_id': kwargs.get('album_id'),
+            'group_id': kwargs.get('group_id'),
+            'v': cfg.VK_API_VERSION
+        }
+
